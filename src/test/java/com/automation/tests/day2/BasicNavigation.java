a5941c1cfeaac4f9b5e31da08010f950301f0db8
metinkara <metinkara@gmail.com>
2020-03-01 13:29:44 -0500

    Basic Navigation Class added

---
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BasicNavigation {
+
+    public static void main(String[] args) {
+
+        // to start selenium script we need:
+        // setup webdriver (browser driver) and create webdriver object
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        // RemoteWebDriver driver = new FirefoxDriver(); // Polymorphism
+        // In Selenium, everything starts from WebDriver interface
+        driver.get("http://google.com"); // to open a website
+    }
+}
